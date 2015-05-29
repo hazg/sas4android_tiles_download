1678e5a4087da69e73f38e929cca6783d7b2d478
hazg <hazg@mail.ru>
2015-05-30 01:55:21 +0300

    remove osmmapMapnik

--- osmmapMapnik/tiles.js
+++
@@ -1,5 +0,0 @@
-function computeUrl(mapName,x,y,z) {
-  var s = "abc".charAt(Math.floor(Math.random()*3));
-  var url = 'http://'+s+'.tile.openstreetmap.org/'+(z-1)+'/'+x+'/'+y+'.png';
-  return url;        
-};
