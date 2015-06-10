8e08c1da1180d68500c42868eb3d1410210dfff5
hazg <hazg@mail.ru>
2015-06-11 00:10:19 +0300

    Navionics

---
+++ navionics_map/tiles.js
@@ -0,0 +1,12 @@
+function computeUrl(mapName,x,y,z) {
+    var s = "abc".charAt(Math.floor(Math.random()*3));
+    var url = 'http://d2hcl9zx8watk4.cloudfront.net/tile/'+(z-1)+'/'+x+'/'+y+'.png?'
+    +'LAYERS=config_1_1_0_0' // Конфигурация параметра config_A_B_C_D:
+    // A - Единица глубины: 1 - метры, 2 - футы
+    // B - Безопасная глубина: 0 - 20 метров, 1 - 10 метров, 2 - 5 метров, 3 - 2 метра, 4 - нет
+    // C - SonarCharts: 0 - выкл, 1 - вкл
+    // D - Ski Map: 0 - Нет, 1 - Да
+    +'&TRANSPARENT=FALSE' // Прозрачность: TRUE или FALSE
+    +'&navtoken=TmF2aW9uaWNzX2ludGVybmFscHVycG9zZV8wMDAwMSt3ZWJhcGl2Mi5uYXZpb25pY3MuY29t';
+    return url;        
+};
