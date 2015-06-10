c9870b364a1ce9b945c310217f956c25cf38bd56
hazg <hazg@mail.ru>
2015-06-10 13:26:40 +0300

    mts, megafon

---
+++ MTS_2G/tiles.js
@@ -0,0 +1,4 @@
+function computeUrl(mapName,x,y,z) {
+  var url = 'http://tiles.mts.ru/G2_New/'+(z-1)+'/'+x+'/'+y;
+  return url;        
+};
---
+++ Megafon_2G/tiles.js
@@ -0,0 +1,4 @@
+function computeUrl(mapName,x,y,z) {
+  var url = 'http://yamap.megafon.ru/g2/'+(z-1)+'/'+x+'/'+y;
+  return url;        
+};
