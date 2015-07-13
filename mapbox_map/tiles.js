6ca2b43c273d1b31b5a4dc0f05fc1d3ccdd5e1b4
hazg <hazg@mail.ru>
2015-07-13 10:09:30 +0300

    sat version

--- mapbox_map/tiles.js
+++ mapbox_map/tiles.js
@@ -1,4 +1,3 @@
-// нет в большом SAS
 function computeUrl(mapName,x,y,z) {
   var s = "abc".charAt(Math.floor(Math.random()*3));
   var url = 'https://'+s+'.tiles.mapbox.com/v3/examples.map-i87786ca/'+(z-1)+'/'+x+'/'+y+'.png';
