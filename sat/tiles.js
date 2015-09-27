c4708e87c70c2ba9519c6a1cec1b5a8bc167c161
hazg <hazg@mail.ru>
2015-09-28 00:13:37 +0300

    push

--- sat/tiles.js
+++ sat/tiles.js
@@ -1,5 +1,5 @@
 function computeUrl(mapName,x,y,z) {
-  var v = 183;
+  var v = 184;
   var url = 'http://khm.google.com/kh/v='+v+'&hl=ru&x='+x+'&y='+y+'&z='+(z-1)+'&s='+("Galileo".charAt(Math.floor(Math.random()*7)));
   return url;             
 };
