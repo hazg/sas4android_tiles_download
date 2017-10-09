3c8c4b5deb9287b1fa6d5fcd8a66e7459043ef63
Aleksandr A <hazg@mail.ru>
2017-10-09 23:25:42 +0300

    push

--- sat/tiles.js
+++ sat/tiles.js
@@ -1,5 +1,5 @@
 function computeUrl(mapName,x,y,z) {
-  var v = 722;
-  var url = 'http://khm3.google.com/kh/v='+v+'&hl=ru&x='+x+'&y='+y+'&z='+(z-1)+'&s='+("Galileo".charAt(Math.floor(Math.random()*7)));
+  var v = 721;
+  var url = 'http://khms3.google.com/kh/v='+v+'&hl=ru&x='+x+'&y='+y+'&z='+(z-1)+'&s=';
   return url;             
 };
