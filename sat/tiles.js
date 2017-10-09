1fb56f89fa564f77b3000649e82bc39c795c4253
Aleksandr A <hazg@mail.ru>
2017-10-09 23:22:13 +0300

    push

--- sat/tiles.js
+++ sat/tiles.js
@@ -1,5 +1,5 @@
 function computeUrl(mapName,x,y,z) {
-  var v = 189;
-  var url = 'http://khm.google.com/kh/v='+v+'&hl=ru&x='+x+'&y='+y+'&z='+(z-1)+'&s='+("Galileo".charAt(Math.floor(Math.random()*7)));
+  var v = 722;
+  var url = 'http://khm3.google.com/kh/v='+v+'&hl=ru&x='+x+'&y='+y+'&z='+(z-1)+'&s='+("Galileo".charAt(Math.floor(Math.random()*7)));
   return url;             
 };
--- topo_marshruty/tiles.js
+++ topo_marshruty/tiles.js
@@ -1,5 +1,5 @@
 function computeUrl(mapName,x,y,z) {
-if (z >= 17) return 'http://localhost';
+  if (z >= 17) return 'http://localhost';
   var url = "http://maps.marshruty.ru/ml.ashx?al=1&i=1"+
     "&x="+x+
     "&y="+y+
